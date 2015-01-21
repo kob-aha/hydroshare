7de9bef9f5570246fa10340652e26e91e7f6b424
shaunjl <shauntheta@gmail.com>
2015-01-20 22:24:06 -0500

    modified:   hs_geo_raster_resource/migrations/0001_initial.py 	modified:   hs_uri_resource/__init__.py 	modified:   hs_uri_resource/admin.py 	modified:   hs_uri_resource/migrations/__init__.py 	modified:   hs_uri_resource/models.py 	new file:   hs_uri_resource/receivers.py 	new file:   hs_uri_resource/templates/pages/create-uri-resource.html 	new file:   hs_uri_resource/templates/pages/uriresource.html 	modified:   hs_uri_resource/tests.py 	modified:   hs_uri_resource/views.py

--- hs_uri_resource/admin.py
+++ hs_uri_resource/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+from mezzanine.pages.admin import PageAdmin
+from django.contrib.gis import admin
+from .models import *
 
-# Register your models here.
+# admin.site.register(MyResource, PageAdmin)
+
+
+admin.site.register(RasterResource, PageAdmin)
---
+++ hs_uri_resource/receivers.py
@@ -0,0 +1,30 @@
+__author__ = 'Hong Yi'
+## Note: this module has been imported in the models.py in order to receive signals
+## at the end of the models.py for the import of this module
+from django.dispatch import receiver
+from hs_core.hydroshare import pre_create_resource, post_create_resource
+from hs_core.signals import *
+from hs_uri_resource.models import UriResource
+
+res_md_dict = {}
+# signal handler to return template contexts
+# to populate create-resource.html template page
+@receiver(pre_describe_resource, sender=UriResource)
+def uri_describe_resource_trigger(sender, **kwargs):
+    if(sender is UriResource):
+        pass
+       
+# signal handler to validate resource metadata, and if valid, retrieve
+# values from create-resource.html page and return a dictionary of metadata_terms to be passed on to
+# hydroshare.create_resource() call to use when creating the resource
+@receiver(pre_call_create_resource, sender=UriResource)
+def raster_pre_call_resource_trigger(sender, **kwargs):
+    if(sender is UriResource):
+        pass
+
+# signal handler to
+@receiver(post_create_resource, sender=UriResource)
+def raster_post_trigger(sender, **kwargs):
+    if sender is UriResource:
+        pass
+
