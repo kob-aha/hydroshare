370f5bb26f6fda297187c30dc4e415f869408084
gantian127 <jamy127@foxmail.com>
2015-04-07 13:15:51 -0400

    [#8]Iss: netcdf tool frontend: header info initial implement

--- hs_app_tools_netCDF/urls.py
+++ hs_app_tools_netCDF/urls.py
@@ -5,6 +5,6 @@ from hs_app_tools_netCDF import views
 urlpatterns = patterns('',
 
     # url for netcdf tools landing page
-    url(r'^nc_tools/index/$', views.index, name='index'),
+    url(r'^nc_tools/index/(?P<shortkey>[A-z0-9]+)/$', views.index, name='index'),
 
 )
--- hs_app_tools_netCDF/views.py
+++ hs_app_tools_netCDF/views.py
@@ -1,7 +1,17 @@
 ﻿from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import get_object_or_404, render_to_response, render
-
+from hs_core.views.utils import authorize
+from hs_app_netCDF.models import NetcdfResource
 
 # views for index page
-def index(request):
-    return render(request, 'pages/nc_tools.html')
+def index(request, shortkey, **kwargs):
+    res, _, _ = authorize(request, shortkey, edit=True, full=True, superuser=True)
+    res_cls = res.__class__
+    context = {}
+    
+    if res_cls is NetcdfResource:
+        context['nc_res'] = res
+    else:
+        context['nc_res'] = None
+
+    return render(request, 'pages/nc_tools.html', context)
