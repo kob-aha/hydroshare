17202a289f331a48b848f867bfa86d002b6ee1d3
gantian127 <jamy127@foxmail.com>
2015-04-21 15:13:32 -0400

    Iss: netcdf tools: modify tool model field meta_edit_file

--- hs_app_tools_netCDF/models.py
+++ hs_app_tools_netCDF/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class NetcdfTools(models.Model):
-    short_id = models.CharField(max_length=50,unique=True)
-    meta_edit_file = models.FileField(blank=True, max_length=1)
+    short_id = models.CharField(max_length=50, unique=True)
+    meta_edit_file = models.FileField(blank=True)
 
     def delete(self, *args, **kwargs):
-        super(NetcdfTools, self).delete(*args,**kwargs)
+        super(NetcdfTools, self).delete(*args, **kwargs)
         if self.meta_edit_file:
             storage, path = self.meta_edit_file.storage, self.meta_edit_file.path
             storage.delete(path)
